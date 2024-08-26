5994cbe5a5c57819f3f45f6229581360d623fe77
Vinícius de Lima Barbosa <159471546+ViniciusLBarbosa@users.noreply.github.com>
2024-08-26 09:14:17 -0300

    Address Finder
    Brazilian address finder by CEP developed in react.js

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco (evento) {
+
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if(cep && cep.length === 8) {
+        //obter o CEP
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+          .then(resposta => resposta.json())
+          .then(dados => {
+            setEndereco(enderecoAntigo => {
+              return{
+                ...enderecoAntigo,
+                rua: dados.logradouro,
+                unidade: dados.unidade,
+                bairro: dados.bairro,
+                cidade: dados.localidade,
+                estado: dados.uf,
+              }
+            })
+          })
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='titulo'>Encontre o endereço pelo CEP</h1>
+        <input className='campo-cep' placeholder='Digite o CEP' onChange={manipularEndereco}></input>
+        <ul className='lista-endereco'>
+          <li>CEP: {endereco.cep}</li>
+          <li>Rua: {endereco.rua}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Unidade: {endereco.unidade}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
